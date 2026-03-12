0d213773fd738cd6e3489d2efd52aeec18d2d09e
fryct999 <fryct999@gmail.com>
2026-03-12 15:00:44 +0300

    RCA-07 create UI file

--- app/src/main/java/ru/fryct999/recipecomposeapp/MainActivity.kt
+++ app/src/main/java/ru/fryct999/recipecomposeapp/MainActivity.kt
@@ -4,40 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ru.fryct999.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RecipeComposeAppTheme {
-                Scaffold { paddingValues ->
-                    Text("Recipes App", modifier = Modifier.padding(paddingValues))
-                }
-            }
+            RecipesApp()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecipeComposeAppTheme {
-        Greeting("Android")
     }
 }
