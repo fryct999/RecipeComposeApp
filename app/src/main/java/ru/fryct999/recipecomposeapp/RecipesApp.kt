623efa05210e5a30d113972e594d32292629b0e4
fryct999 <fryct999@gmail.com>
2026-03-14 10:46:42 +0300

    RCA-09 fix

--- app/src/main/java/ru/fryct999/recipecomposeapp/RecipesApp.kt
+++ app/src/main/java/ru/fryct999/recipecomposeapp/RecipesApp.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import ru.fryct999.recipecomposeapp.ui.categories.CategoriesScreen
@@ -13,7 +14,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ru.fryct999.recipecomposeapp.ui.navigation.BottomNavigation
 import ru.fryct999.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 
