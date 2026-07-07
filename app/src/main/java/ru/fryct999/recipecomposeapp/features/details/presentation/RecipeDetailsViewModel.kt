73ccfb928d8ee3b4b3dbb2f6a035d3be5a7b8408
fryct999 <fryct999@gmail.com>
2026-07-07 05:18:17 +0300

    RCA-28 create favorites and details viewmodel

---
+++ app/src/main/java/ru/fryct999/recipecomposeapp/features/details/presentation/RecipeDetailsViewModel.kt
@@ -0,0 +1,97 @@
+package ru.fryct999.recipecomposeapp.features.details.presentation
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import ru.fryct999.recipecomposeapp.core.utils.FavoriteDataStoreManager
+import ru.fryct999.recipecomposeapp.data.repository.RecipesRepository
+import ru.fryct999.recipecomposeapp.features.details.presentation.model.RecipeDetailsUiState
+import ru.fryct999.recipecomposeapp.features.recipes.presentation.model.IngredientUiModel
+import ru.fryct999.recipecomposeapp.features.recipes.presentation.model.RecipeUiModel
+import ru.fryct999.recipecomposeapp.features.recipes.presentation.model.toUiModel
+import ru.fryct999.recipecomposeapp.navigation.Constants.PARAM_RECIPE_ID
+
+class RecipeDetailsViewModel(
+    application: Application,
+    savedStateHandle: SavedStateHandle,
+    recipesRepository: RecipesRepository,
+) : AndroidViewModel(application) {
+    private val favoriteManager = FavoriteDataStoreManager(application)
+    private val recipeId = savedStateHandle.get<Int>(PARAM_RECIPE_ID) ?: -1
+    private val _uiState = MutableStateFlow(RecipeDetailsUiState())
+    val uiState: StateFlow<RecipeDetailsUiState> = _uiState.asStateFlow()
+
+    init {
+        setLoading(true)
+        viewModelScope.launch {
+            try {
+                val recipe = recipesRepository.getRecipeById(recipeId)
+                val ingredientsList = recipe?.ingredients?.map { ingredient ->
+                    ingredient.toUiModel()
+                } ?: emptyList()
+
+                setRecipe(recipe?.toUiModel())
+                setPortionCount(1)
+                setIngredients(ingredientsList)
+                setLoading(false)
+            } catch (e: Exception) {
+                setError("Ошибка при загрузке списка ингредиентов.")
+                setLoading(false)
+            }
+        }
+
+        viewModelScope.launch {
+            favoriteManager.getFavoriteIdsFlow()
+                .collect { favoriteIds ->
+                    val isFavorite = favoriteIds.contains(recipeId.toString())
+                    _uiState.update { it.copy(isFavorite = isFavorite) }
+                }
+        }
+    }
+
+    private fun setLoading(loading: Boolean) {
+        _uiState.update { currentState ->
+            currentState.copy(isLoading = loading)
+        }
+    }
+
+    private fun setError(error: String) {
+        _uiState.update { currentState ->
+            currentState.copy(error = error)
+        }
+    }
+
+    private fun setRecipe(recipe: RecipeUiModel?) {
+        _uiState.update { currentState ->
+            currentState.copy(recipe = recipe)
+        }
+    }
+
+    private fun setIngredients(ingredients: List<IngredientUiModel>) {
+        _uiState.update { currentState ->
+            currentState.copy(ingredients = ingredients)
+        }
+    }
+
+    fun setPortionCount(portionsCount: Int) {
+        _uiState.update { currentState ->
+            currentState.copy(portionsCount = portionsCount)
+        }
+    }
+
+    fun toggleFavorite() {
+        viewModelScope.launch {
+            if (_uiState.value.isFavorite) {
+                favoriteManager.removeFavorite(recipeId)
+            } else {
+                favoriteManager.addFavorite(recipeId)
+            }
+        }
+    }
+}
--- app/src/main/java/ru/fryct999/recipecomposeapp/features/favorites/ui/FavoritesScreen.kt
+++ app/src/main/java/ru/fryct999/recipecomposeapp/features/favorites/ui/FavoritesScreen.kt
@@ -1,49 +1,38 @@
 package ru.fryct999.recipecomposeapp.features.favorites.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import kotlinx.coroutines.flow.map
+import androidx.lifecycle.viewmodel.compose.viewModel
 import ru.fryct999.recipecomposeapp.R
 import ru.fryct999.recipecomposeapp.core.ui.ScreenHeader
-import ru.fryct999.recipecomposeapp.core.utils.FavoriteDataStoreManager
-import ru.fryct999.recipecomposeapp.data.repository.RecipesRepository
-import ru.fryct999.recipecomposeapp.data.repository.RecipesRepositoryStub
+import ru.fryct999.recipecomposeapp.features.favorites.presentation.FavoritesViewModel
 import ru.fryct999.recipecomposeapp.features.recipes.ui.RecipeItem
-import ru.fryct999.recipecomposeapp.features.recipes.presentation.model.toUiModel
 import ru.fryct999.recipecomposeapp.ui.theme.Dimens.padding16
 import ru.fryct999.recipecomposeapp.ui.theme.Dimens.padding8
 import ru.fryct999.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 
 @Composable
 fun FavoritesScreen(
-    favoriteDataStoreManager: FavoriteDataStoreManager,
-    recipesRepository: RecipesRepository,
     onRecipeClick: (Int) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val recipes by remember {
-        favoriteDataStoreManager.getFavoriteIdsFlow().map { ids ->
-            ids.mapNotNull { id ->
-                recipesRepository.getRecipeById(id.toIntOrNull() ?: return@mapNotNull null)
-                    ?.toUiModel()
-            }
-        }
-    }.collectAsState(initial = emptyList())
+    val viewModel: FavoritesViewModel = viewModel()
+    val uiState by viewModel.uiState.collectAsState()
 
     Column(
         modifier = modifier
@@ -54,27 +43,46 @@ fun FavoritesScreen(
             text = "ИЗБРАННОЕ",
         )
 
-        if (recipes.isEmpty()) {
-            Column(
+        if (uiState.isLoading) {
+            Box(
                 modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        } else if (uiState.error != null) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
             ) {
                 Text(
-                    text = "Список избранного сейчас пуст...",
-                    style = MaterialTheme.typography.displayLarge,
+                    text = uiState.error ?: "Непредвиденная ошибка",
+                    style = MaterialTheme.typography.titleMedium,
                 )
             }
         } else {
-            LazyColumn(
-                modifier = Modifier.weight(1f)
-            ) {
-                items(recipes, key = { it.id }) { recipe ->
-                    RecipeItem(
-                        recipe = recipe,
-                        onClick = { onRecipeClick(recipe.id) },
-                        modifier = Modifier.padding(horizontal = padding16, vertical = padding8)
+            if (uiState.isEmpty) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                ) {
+                    Text(
+                        text = "Список избранного сейчас пуст...",
+                        style = MaterialTheme.typography.displayLarge,
                     )
+                }
+            } else {
+                LazyColumn(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    items(uiState.favoriteRecipes, key = { it.id }) { recipe ->
+                        RecipeItem(
+                            recipe = recipe,
+                            onClick = { onRecipeClick(recipe.id) },
+                            modifier = Modifier.padding(horizontal = padding16, vertical = padding8)
+                        )
+                    }
                 }
             }
         }
@@ -86,8 +94,6 @@ fun FavoritesScreen(
 fun PreviewFavoritesScreen() {
     RecipeComposeAppTheme {
         FavoritesScreen(
-            favoriteDataStoreManager = FavoriteDataStoreManager(LocalContext.current),
-            recipesRepository = RecipesRepositoryStub,
             onRecipeClick = { _ -> },
             modifier = Modifier,
         )
