9e1fef3e440451e55d4dda4a0754b80fb2066257
fryct999 <fryct999@gmail.com>
2026-08-14 15:03:22 +0300

    RCA-50 fix

--- app/src/androidTest/java/ru/fryct999/recipecomposeapp/features/categories/ui/CategoriesContentTest.kt
+++ app/src/androidTest/java/ru/fryct999/recipecomposeapp/features/categories/ui/CategoriesContentTest.kt
@@ -5,12 +5,15 @@ import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 import ru.fryct999.recipecomposeapp.features.categories.presentation.model.CategoriesUiState
 import ru.fryct999.recipecomposeapp.features.categories.presentation.model.CategoryUiModel
 
+@RunWith(AndroidJUnit4::class)
 class CategoriesContentTest {
     @get:Rule
     val composeTestRule = createComposeRule()
--- app/src/androidTest/java/ru/fryct999/recipecomposeapp/features/recipes/ui/RecipesContentTest.kt
+++ app/src/androidTest/java/ru/fryct999/recipecomposeapp/features/recipes/ui/RecipesContentTest.kt
@@ -5,11 +5,14 @@ import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 import ru.fryct999.recipecomposeapp.features.recipes.presentation.model.RecipeUiModel
 import ru.fryct999.recipecomposeapp.features.recipes.presentation.model.RecipesUiState
 
+@RunWith(AndroidJUnit4::class)
 class RecipesContentTest {
     @get:Rule
     val composeTestRule = createComposeRule()
